55ebf44885e717ebfa0965e4731bde97e5f2d6de
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 20:09:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,10 @@
+def using_concat(my_favorite_things, more_favs)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  @all_my_favs = @my_favorite_things.concat(more_favs)
+end
+
+def using_insert(@list_of_programming_languages, @another_language)
+  new_array = using_insert(@list_of_programming_languages, @another_language)
+  new_array[4]
+end
