e9447cd5df3874355265643c5b721dc6a45e9299
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 20:11:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,7 @@ def using_concat(my_favorite_things, more_favs)
   @all_my_favs = @my_favorite_things.concat(more_favs)
 end
 
-def using_insert(@list_of_programming_languages, @another_language)
-  new_array = using_insert(@list_of_programming_languages, @another_language)
+def using_insert(list_of_programming_languages, another_language)
+  new_array = using_insert(list_of_programming_languages, another_language)
   new_array[4]
 end
